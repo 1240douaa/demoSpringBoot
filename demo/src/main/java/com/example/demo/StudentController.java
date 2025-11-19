6094fc399dbbc2ce2142895c24e15ff767340f16
doua <douah503@gmail.com>
2025-11-19 08:18:24 +0100

    first

---
+++ demo/src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+    
+    @Autowired
+    private StudentRepository studentRepository;
+    
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+    
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+    
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+}
